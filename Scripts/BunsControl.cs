eae6a1ba1994abfdf51319509717cce26ded5800
Liuei Feng <jadur.d@gmail.com>
2019-06-14 16:39:04 +0900

    Update No.01
    20190614_1632

---
+++ Scripts/BunsControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunsControl : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("aaa");
+    }
+}
